676346924ac49724385a0ead02e4cd32a2d08cfb
Aurel Nicolae <32492300+the1howie@users.noreply.github.com>
2024-11-02 15:20:09 +0000

    Add files via upload
    Some fixes
    
    Signed-off-by: Aurel Nicolae <32492300+the1howie@users.noreply.github.com>

--- Pythagorean-Theorem-Visual-Proof/pythag-visual-proof.m
+++ Pythagorean-Theorem-Visual-Proof/pythag-visual-proof.m
@@ -2,13 +2,27 @@
 %% Author: Aurel Nicolae 2019
 %% Plot Pythagorean Theorem Concept
 clear; close all;
-% INPUT the lengths of the adjacent sides to the 90 degrees angle:
-a = 4; b = 3; % a = 4; b = 3; % a = 12; b = 5; % a = 15; b = 8; % a=1; b=1;
-c = sqrt(a^2 + b^2); % hypothenuse
+
+% Input the lengths of the smaller sides adjacent to the 90 degrees angle:
+a = 4;
+b = 3;
+% Alternatively, you can use the input() function.
+% a = input("Type in the length for side a: ");
+% b = input("Type in the length for side b: ");
+% This requires some validation.
+
+
+% Calculate the hypothenuse
+c = sqrt(a^2 + b^2);
 fprintf("Hypothenuse is %.4f\n", c);
 
-figure;
+
+% Plot the figure occupying the whole screen
+%figure(1, "position", get(0, "screensize"))
+
+
 % Plot original right triangle
+subplot(2, 2, 1);
 x1=[0,a,a,0];
 y1=[0,0,b,0];
 plot(x1,y1); fill(x1,y1,[0, 0.5, 0]); hold on; axis equal;
@@ -22,10 +36,10 @@ plot(x3,y3); fill(x3,y3,[1,1,0]);hold on; axis equal;
 x4=[0,a,a-b,-b,0];
 y4=[0,b,a+b,a,0];
 plot(x4,y4); fill(x4,y4,[1,0,0]);hold off; axis equal;
-grid on; title("Pythagorean Theorem: a^2 + b^2 = c^2");
+grid on; title("1. Pythagorean Theorem: a^2 + b^2 = c^2");
 
 %% Proof of Pythagorean Theorem
-figure;
+subplot(2, 2, 2);
 % Build an (a+b)-by-(a+b) perfect square by copying and pasting clockwise
 % the 4 permuations of the original right angle triangle with sides a, b, c.
 x1=[0,a,a,0];
@@ -43,13 +57,13 @@ plot(x4,y4); fill(x4,y4,[0, 0.5, 0]); hold on; axis equal;
 x5=[0,a,a-b,-b,0];
 y5=[0,b,a+b,a,0];
 plot(x5,y5); fill(x5,y5,[1,0,0]); hold off; axis equal;
-grid on; title("Build (a+b)-by-(a+b) perfect square, area: (a+b)^2 = 2ab + c^2");
+grid on; title("2. Build (a+b)-by-(a+b) perfect square where area: (a+b)^2 = 2ab + c^2");
 
 %% Shuffle the right angle triangles inside the perfect square
 % NOTE: the area inside is c^2, while the remaining area is 2*a*b.
 % The remaining area measures 2ab because you can build 2 rectangles
 % which are a-by-b each, with areas a*b respectively.
-figure;
+subplot(2, 2, 3);
 xsq=[a,a,-b,-b,a];
 ysq=[0,a+b,a+b,0,0];
 plot(xsq,ysq,'b-'); hold on; axis equal;
@@ -84,14 +98,14 @@ p2 = [-b/3, a/3];    % Second Point
 dp = p2-p1;         % Difference
 quiver(p1(1),p1(2),dp(1),dp(2),0,'MaxHeadSize',0.2,'color',[1,0,0],'LineWidth',1.5);
 hold off; axis equal;
-grid on; title("Re-arranged (a+b)-by-(a+b) perfect square, area: (a+b)^2 unchanged");
+grid on; title("3. Re-arranged (a+b)-by-(a+b) perfect square, area: (a+b)^2 unchanged");
 
 %% Perfect square (a+b)^2 = a^2 + 2ab + b^2
 % we've seen above that the perfect square was composed in such a way
 % that the area (a+b)^2 = 2ab + c^2
 % Thus from these two equations, we obtain
 % that a^2 + b^2 = c^2. Voila!
-figure;
+subplot(2, 2, 4);
 xsq=[a,a,-b,-b,a];
 ysq=[0,a+b,a+b,0,0];
 plot(xsq,ysq,'b-'); hold on; axis equal;
@@ -113,5 +127,5 @@ plot(xa2,ya2); fill(xa2,ya2,[0,0,1]); hold on; axis equal;
 xb2=[a,a,a-b,a-b,a];
 yb2=[a,a+b,a+b,a,a];
 plot(xb2,yb2); fill(xb2,yb2,[1,1,0]); hold off; axis equal; grid on;
-title("Proof that a^2 + b^2 = c^2 since area (a+b)^2 = 2ab + c^2 = a^2 + 2ab + b^2");
+title("4. Proof that a^2 + b^2 = c^2 since area 2ab + c^2 = a^2 + 2ab + b^2");
 % End of proof.
